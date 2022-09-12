09eaa228bbcfc4016f1e20fa542abb1d7e7a638a
Bharath Babu <bharathbabu68@gmail.com>
2022-09-12 12:27:43 +0400

    Initial commit

---
+++ rates.py
@@ -0,0 +1,33 @@
+from forex_python.converter import CurrencyRates
+
+
+def get_live_exchange_rate(curr1, curr2):
+    '''
+    This function returns the live exchange rate between a pair of currencies
+
+    Parameters:
+    - curr1 (string) : First currency in the pair for getting exchange rate
+    - curr2 (string) : Second currency in the pair for getting exchange rate
+
+    Return:
+    - float : Current Exchange rate
+    '''
+    c = CurrencyRates()
+    exchange_rate = c.get_rate(curr1, curr2) 
+    return exchange_rate
+
+def convert_currencies(from_curr, to_curr, amount):
+    '''
+    This function converts a given amount from one currency to another
+
+    Parameters:
+    - from_curr (string) : First currency in the pair for conversion
+    - to_curr (string) : Second currency in the pair for conversion
+    - amount: Amount / value of currency to be converted
+
+    Return:
+    - float : Converted Value
+    '''
+    c = CurrencyRates()
+    converted_value = c.convert(from_curr, to_curr, amount)
+    return converted_value
