35a16d30e1e51dc46daa96a798406c9d56a2dc6d
Bharath Babu <bharathbabu68@gmail.com>
2022-09-12 13:00:04 +0400

    Update rates.py

--- rates.py
+++ rates.py
@@ -32,6 +32,3 @@ def convert_currencies(from_curr, to_curr, amount):
     exchange_rate = get_live_exchange_rate(from_curr, to_curr)
     converted_value = exchange_rate * amount
     return converted_value
-
-res = convert_currencies("USD", "EUR", 200)
-print(res)
